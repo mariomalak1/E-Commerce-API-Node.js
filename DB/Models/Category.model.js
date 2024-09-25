84cc03f9f711ccf12ca14873db52679cb004eb1d
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-09-25 03:13:30 +0300

    create category model - still image

--- DB/Models/Category.model.js
+++ DB/Models/Category.model.js
@@ -1,7 +1,17 @@
 import mongoose from "mongoose";
 
 const CategorySchema = mongoose.Schema({
-    name: String
-});
+    name: {
+        type: String,
+        required: [true, "category name is required"],
+        unique: [true, "category name must be unique"],
+        minLength: [3, "too short category name"],
+        maxLength: [36, "too long category name"]
+    },
+    slug:{
+        type: String,
+        lowercase: true
+    }
+}, {timestamps: true});
 
 export const CategoryModel = mongoose.model("Category", CategorySchema);
