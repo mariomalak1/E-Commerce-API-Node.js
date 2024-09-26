5b4769c09d39c35e80d48b215387372a4f578c16
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-09-27 01:09:58 +0300

    create all subcategory controllers

--- SRC/Controllers/Category.controller.js
+++ SRC/Controllers/Category.controller.js
@@ -1,9 +1,8 @@
 import expressAsyncHandler from "express-async-handler";
 import slugify from "slugify";
 
-import {CategoryModel} from "../../DB/Models/Category.model.js";
-import {ApiError} from "../Utillis/apiErrors.js";
-
+import { CategoryModel } from "../../DB/Models/Category.model.js";
+import { ApiError } from "../Utillis/apiErrors.js";
 
 // @desc    Get list of categories
 // @route   GET api/v1/categories
@@ -14,81 +13,91 @@ export const getCategoryController = expressAsyncHandler(async (req, res) => {
 
     let skip = (page - 1) * limit;
     const numberOfCategories = await CategoryModel.countDocuments();
-    if(numberOfCategories < skip || skip < 0 ){
+    if (numberOfCategories < skip || skip < 0) {
         page = 1;
         skip = 0;
     }
     const categories = await CategoryModel.find().skip(skip).limit(limit);
     res.status(200).send({
-        meta:{
-            results:categories.length,
-            currentPage:page,
-            limit:limit,
+        meta: {
+            results: categories.length,
+            currentPage: page,
+            limit: limit,
         },
-        data:categories
+        data: categories,
     });
 });
 
 // @desc    Create category
 // @route   POST api/v1/categories
 // @access  private
-export const postCategoryController = expressAsyncHandler(async (req, res, next) => {
-    let name = req.body.name;
-    const category = await CategoryModel.create({name, slug:slugify(name)});
-    res.status(201).send({"data":category})
-});
+export const postCategoryController = expressAsyncHandler(
+    async (req, res, next) => {
+        let name = req.body.name;
+        const category = await CategoryModel.create({
+            name,
+            slug: slugify(name),
+        });
+        res.status(201).send({ data: category });
+    }
+);
 
 // @desc    Get specific category by id
 // @route   GET api/v1/categories/{id}
 // @access  public
-export const getCategoryByIdController = expressAsyncHandler(async (req, res, next) =>{
-    const {id} = req.params;
+export const getCategoryByIdController = expressAsyncHandler(
+    async (req, res, next) => {
+        const { id } = req.params;
 
-    let category = await CategoryModel.findById(id);
+        let category = await CategoryModel.findById(id);
 
-    if(!category){
-        return next(new ApiError(`category not found with id: ${id}`, 404));
+        if (!category) {
+            return next(new ApiError(`category not found with id: ${id}`, 404));
+        }
+
+        res.status(200).send({ data: category });
     }
-
-    res.status(200).send({data:category});
-});
+);
 
 // @ desc   Update specific category
 // @route   PUT api/v1/categories/{id}
 // @access  private
-export const updateSpecificCategoryController = expressAsyncHandler(async (req, res) => {
-    const {id} = req.params;
-    const {name} = req.body;
-    
-    if(!name){
-        return next(new ApiError("name is required", 400));
-        // res.status(400).send({"msg": ""});
+export const updateSpecificCategoryController = expressAsyncHandler(
+    async (req, res) => {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        if (!name) {
+            return next(new ApiError("name is required", 400));
+            // res.status(400).send({"msg": ""});
+        }
+        const category = await CategoryModel.findOneAndUpdate(
+            { _id: id },
+            { name, slug: slugify(name) },
+            { returnOriginal: false }
+        );
+
+        if (!category) {
+            return next(new ApiError(`category not found with id: ${id}`, 404));
+        }
+
+        res.status(200).send({ data: category });
     }
-    const category = await CategoryModel.findOneAndUpdate(
-        {_id: id}, 
-        {name, slug: slugify(name)},
-        {returnOriginal: false}
-    );
-
-    if(!category){
-        return next(new ApiError(`category not found with id: ${id}`, 404));
-    }
-
-    res.status(200).send({data: category});
-});
-
+);
 
 // @desc    DELETE specific category with id
 // @route   DELTE api/v1/categories/{id}
 // @access  private
-export const deleteCategoryController = expressAsyncHandler(async (req, res) => {
-    const {id} = req.params;
+export const deleteCategoryController = expressAsyncHandler(
+    async (req, res) => {
+        const { id } = req.params;
 
-    const category = await CategoryModel.findByIdAndDelete(id);
-    
-    if(!category){
-        return next(new ApiError(`category not found with id: ${id}`, 404));
+        const category = await CategoryModel.findByIdAndDelete(id);
+
+        if (!category) {
+            return next(new ApiError(`category not found with id: ${id}`, 404));
+        }
+
+        res.status(204).send();
     }
-
-    res.status(204).send();
-});
+);
---
+++ SRC/Controllers/subCategory.controller.js
@@ -0,0 +1,113 @@
+import expressAsyncHandler from "express-async-handler";
+import slugify from "slugify";
+
+import SubCategoryModel from "../../DB/Models/SubCategory.model.js";
+import { ApiError } from "../Utillis/apiErrors.js";
+
+// @desc    Get list of subCategories
+// @route   GET api/v1/subCategories
+// @access  public
+export const getSubCategoryController = expressAsyncHandler(
+    async (req, res) => {
+        let page = req.query.page * 1 || 1;
+        let limit = req.query.limit * 1 || 10;
+
+        let skip = (page - 1) * limit;
+        const numberOfSubCategories = await SubCategoryModel.countDocuments();
+        if (numberOfSubCategories < skip || skip < 0) {
+            page = 1;
+            skip = 0;
+        }
+        const subCategories = await SubCategoryModel.find()
+            .skip(skip)
+            .limit(limit);
+
+        res.status(200).send({
+            meta: {
+                results: subCategories.length,
+                currentPage: page,
+                limit: limit,
+            },
+            data: subCategories,
+        });
+    }
+);
+
+// @desc    Create subCategory
+// @route   POST api/v1/subCategories
+// @access  private
+export const postSubCategoryController = expressAsyncHandler(
+    async (req, res) => {
+        const { name, category } = req.body;
+
+        const subCategory = await SubCategoryModel.create({
+            name,
+            category,
+            slug: slugify(name),
+        });
+
+        res.status(201).send({ data: subCategory });
+    }
+);
+
+// @desc    Get specific subCategory by id
+// @route   GET api/v1/subCategories/{id}
+// @access  public
+export const getSubCategoryByIdController = expressAsyncHandler(
+    async (req, res, next) => {
+        const { id } = req.params;
+
+        let subCategory = await SubCategoryModel.findById(id);
+
+        if (!subCategory) {
+            return next(
+                new ApiError(`subCategory not found with id: ${id}`, 404)
+            );
+        }
+
+        res.status(200).send({ data: subCategory });
+    }
+);
+
+// @ desc   Update specific subCategory
+// @route   PUT api/v1/subCategories/{id}
+// @access  private
+export const updateSpecificSubCategoryController = expressAsyncHandler(
+    async (req, res) => {
+        const { id } = req.params;
+        const { name, category } = req.body;
+
+        const subCategory = await SubCategoryModel.findOneAndUpdate(
+            { _id: id },
+            { name, slug: slugify(name), category },
+            { returnOriginal: false }
+        );
+
+        if (!subCategory) {
+            return next(
+                new ApiError(`subCategory not found with id: ${id}`, 404)
+            );
+        }
+
+        res.status(200).send({ data: subCategory });
+    }
+);
+
+// @desc    DELETE specific subCategory with id
+// @route   DELTE api/v1/subCategories/{id}
+// @access  private
+export const deleteSubCategoryController = expressAsyncHandler(
+    async (req, res) => {
+        const { id } = req.params;
+
+        const subCategory = await SubCategoryModel.findByIdAndDelete(id);
+
+        if (!subCategory) {
+            return next(
+                new ApiError(`subCategory not found with id: ${id}`, 404)
+            );
+        }
+
+        res.status(204).send();
+    }
+);
