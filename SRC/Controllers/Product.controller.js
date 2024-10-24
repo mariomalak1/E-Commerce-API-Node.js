96688fe329f1eb703eb63a1a0fb1827d8414b1e7
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-10-25 02:08:04 +0300

    create curd for product

---
+++ SRC/Controllers/Product.controller.js
@@ -0,0 +1,97 @@
+/* eslint-disable no-undef */
+import expressAsyncHandler from "express-async-handler";
+import slugify from "slugify";
+
+import { ProductModel } from "../../DB/Models/Product.model.js";
+import { ApiError } from "../Utillis/apiErrors.js";
+
+// @desc    Get list of products
+// @route   GET api/v1/products
+// @access  public
+export const getProductsController = expressAsyncHandler(async (req, res) => {
+    let page = req.query.page * 1 || 1;
+    let limit = req.query.limit * 1 || 10;
+
+    let skip = (page - 1) * limit;
+    const numberOfProducts = await ProductModel.countDocuments();
+    if (numberOfProducts < skip || skip < 0) {
+        page = 1;
+        skip = 0;
+    }
+    const products = await ProductModel.find().skip(skip).limit(limit);
+    res.status(200).send({
+        meta: {
+            results: products.length,
+            currentPage: page,
+            limit: limit,
+        },
+        data: products,
+    });
+});
+
+// @desc    Create product
+// @route   POST api/v1/products
+// @access  private
+export const postProductController = expressAsyncHandler(
+    async (req, res) => {
+        req.body.slug = slugify(req.body.title);
+        const product = await ProductModel.create(req.body);
+        res.status(201).send({ data: product });
+    }
+);
+
+// @desc    Get specific product by id
+// @route   GET api/v1/products/{id}
+// @access  public
+export const getProductByIdController = expressAsyncHandler(
+    async (req, res, next) => {
+        const { id } = req.params;
+
+        let product = await ProductModel.findById(id);
+
+        if (!product) {
+            return next(new ApiError(`product not found with id: ${id}`, 404));
+        }
+
+        res.status(200).send({ data: product });
+    }
+);
+
+// @ desc   Update specific product
+// @route   PUT api/v1/products/{id}
+// @access  private
+export const updateSpecificProductController = expressAsyncHandler(
+    async (req, res) => {
+        req.body.slug = slugify(req.body.title);
+
+        const { id } = req.params;
+
+        const product = await ProductModel.findOneAndUpdate(
+            { _id: id },
+            req.body,
+            { returnOriginal: false }
+        );
+
+        if (!product) {
+            return next(new ApiError(`product not found with id: ${id}`, 404));
+        }
+
+        res.status(200).send({ data: product });
+    }
+);
+
+// @desc    DELETE specific product with id
+// @route   DELTE api/v1/products/{id}
+// @access  private
+export const deleteProductController = expressAsyncHandler(
+    async (req, res, next) => {
+        const { id } = req.params;
+
+        const product = await ProductModel.findByIdAndDelete(id);
+
+        if (!product) {
+            return next(new ApiError(`product not found with id: ${id}`, 404));
+        }
+        res.status(204).send();
+    }
+);
